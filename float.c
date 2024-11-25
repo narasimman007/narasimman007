53d352cc42984482a0c35eb392a4833749d1da13
narasimman007 <narasimmanmanju7@gmail.com>
2024-11-25 11:52:01 +0530

    Add files via upload

---
+++ float.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,x;
+    printf("enter float value 1\n");
+    scanf("%f",&a);
+    printf("enter float value 2\n");
+    scanf("%f",&b);
+    x=a*b;
+    printf("mutification value:%f",x);
+}
