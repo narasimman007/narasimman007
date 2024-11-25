9e6842ad0d4f0d7667e1b90fb2aa79b634ad64ac
narasimman007 <narasimmanmanju7@gmail.com>
2024-11-25 11:37:25 +0530

    Add files via upload

---
+++ arithmatic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("sum = %d\n",a+b);
+    printf("subtraction = %d\n",a-b);
+    printf("multiplication = %d\n",a*b);
+    printf("division = %d\n",a/b);
+}
+
+
+
