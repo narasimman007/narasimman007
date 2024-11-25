1006108e7bf948e36f5336bb4598b6191a057849
narasimman007 <narasimmanmanju7@gmail.com>
2024-11-25 11:47:42 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main ()
+{
+    short int a;
+    long int b ;
+    long long int c ;
+    double d ;
+    float e;
+    long double f ;
+    
+    printf ("Size Of Short = %d Bytes \n",sizeof(a));
+    printf ("Size Of Long = %d Bytes \n",sizeof(b));
+    printf ("Size Of Long long = %d Bytes \n",sizeof(c));
+    printf ("Size Of Double = %d Bytes \n",sizeof(d));
+    printf ("Size Of Float = %d Bytes \n",sizeof(e));
+    printf ("Size Of Long Double = %d Bytes \n",sizeof(f));
+}
+ 
+  
+  
+  
+
