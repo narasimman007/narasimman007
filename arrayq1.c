7bdf09ed950823c6256abdc8f148d2ea3775150d
narasimman007 <narasimmanmanju7@gmail.com>
2024-11-25 11:42:21 +0530

    Add files via upload

---
+++ arrayq1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int values[5];
+    printf("enter 5 integer:");
+    for(int i=0;i<5;++i){
+        scanf("%d",& values[i]);
+    }
+    printf("displaying integers:");
+    for (int i=0;i<5;++i){
+        printf("%d\n",values[i]);
+    }
+    return 0;
+}
+
+
+
+
